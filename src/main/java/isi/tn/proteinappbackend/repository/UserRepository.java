ef23ee1e9b0257a5e10c30aac0faf94b45b33255
Seyfeddine Jouini <jouini.seyf.js@gmail.com>
2024-05-11 18:30:42 +0100

    Update UserRepository.java

--- src/main/java/isi/tn/proteinappbackend/repository/UserRepository.java
+++ src/main/java/isi/tn/proteinappbackend/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+  public List<User> findAllUsers();
+    public User findUserById(Long id);
+    public String remove(Long id);
+    public String updateUser(Long id, User user);
 }
